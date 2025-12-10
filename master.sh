4435554247ba57ac63c7400fc26e7fd2e939382f
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2025-12-10 02:58:49 +0200

    Updated master script

--- master.sh
+++ master.sh
@@ -1,7 +1,11 @@
 cat working.log | grep -v GPRMC > 2.log
 cat working.log | grep GPRMC > 1.log
 
+python3 parse-coords.py > 1_parsed.log
+cat 2.log | awk -F" " '{print $1","$2}' > 2_parsed.log
+
 echo "time,lat,lng,mean" > result.log
+
 
 python3 combine.py >> result.log
 
